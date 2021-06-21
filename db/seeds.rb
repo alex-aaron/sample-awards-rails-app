bc481c26e014f85ec6956e228a58907bf603f275
Alex <alexfrancisaaron@gmail.com>
2021-06-21 17:18:32 -0500

    added initial seed data and checked console

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(username: "User1", password: "password1")
-user2 = User.create(username: "User2", password: "password2")
-user_award1 = UserAward.create()
+user1 = NonDeviseUser.create(username: "User1", password: "password1")
+user2 = NonDeviseUser.create(username: "User2", password: "password2")
+user_award1 = UserAward.create(year: 1992, non_devise_user_id: 1)
+user_award2 = UserAward.create(year: 1986, non_devise_user_id: 2)
+category1 = Category.create(user_award_id: 1, nominees: "Unforgiven/ Malcolm X / The Player", winner: "Unforgiven", name: "Best Picture")
+category2 = Category.create(user_award_id: 2, nominees: "Blue Velvet/ Hannah and Her Sisters/ Big Trouble in Little China", winner: "Blue Velvet", name: "Best Picture")
