f4e35904fc17945f1f639550da32c8aa898535df
Alex <alexfrancisaaron@gmail.com>
2021-06-24 14:11:19 -0500

    created static controller, view, added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :non_devise_users
   resources :user_awards
   resources :categories
+  get '/' => "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
